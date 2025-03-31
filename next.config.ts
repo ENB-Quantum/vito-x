e1ffa3fe168469df555f056ff7083920274ff034
rahularya002 <ritiksaini258@gmail.com>
2025-03-31 20:42:43 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [], // If you need this option
 };
 
 export default nextConfig;
